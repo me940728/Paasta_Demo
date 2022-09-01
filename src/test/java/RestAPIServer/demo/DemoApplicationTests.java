55345f78f20e56de14dc30f8aaf4919b3c09eb91
Officer_Choi <me940728@naver.com>
2022-09-01 19:53:52 +0900

    [UPDATE] testCode

--- src/test/java/RestAPIServer/demo/DemoApplicationTests.java
+++ src/test/java/RestAPIServer/demo/DemoApplicationTests.java
@@ -7,15 +7,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 class DemoApplicationTests {
 
 	@Test
-	void 로그인테스트하기() throws Exception{
+	void 이름확인() throws Exception{
 		// Given
-		String admin = "admin";
-		String password = "1234";
-
+		String name = "최별규";
+		Boolean bol = null;
 		// When
-
+		bol = name.equals("최별규");
 		// Then
-
+		System.out.print(bol == true? "이름은 최별규 입니다." : "다른 사람입니다.");
 	}
 
 }
